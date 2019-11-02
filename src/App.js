3c622ef13615450fc0f41143618dcc1a3019d7d2
Lyuben Dimitrov <lyuben.dimitrov.ext@pentasys.de>
2019-11-03 00:14:21 +0100

    Added a query to get 150 pokemons and display it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useQuery } from '@apollo/react-hooks';
+import gql from "graphql-tag";
 import './App.css';
 
+const GET_POKEMON_INFO = gql`
+{
+    pokemons(first: 150) {
+      id
+      number
+      name,
+      image,  
+      evolutions {
+        id,
+        number,
+        name,
+        image
+      }
+    }
+  }`
+
 function App() {
+  const { data, loading, error } = useQuery(GET_POKEMON_INFO);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>Pokemons</h1>
+      <div>
+        {data &&
+          data.pokemons &&
+          data.pokemons.map((pokemon, index) => (
+            <div key={index}>
+              <img src={pokemon.image} alt="pokomen" />
+              <div>
+                <h3>{pokemon.name}</h3>
+                <p>
+                  {pokemon.evolutions && pokemon.evolutions.length !== 0 && (
+                    <p>
+                      {" "}
+                      Evolutions:
+                    {pokemon.evolutions.map((e, indx) => {
+                        return <p key={indx}> {e.name} </p>;
+                      })}
+                    </p>
+                  )}
+                </p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </React.Fragment>
   );
 }
 
